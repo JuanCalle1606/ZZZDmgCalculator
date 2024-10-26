89fb58ec6b02b683d0acf5e19a7c69423b0c4c29
JuanCalle1606 <jpmilojp@gmail.com>
2024-10-26 13:36:06 -0500

    Added utility buttons to AgentsView.razor

--- ZZZDmgCalculator/Components/Setup/AgentsView.razor.cs
+++ ZZZDmgCalculator/Components/Setup/AgentsView.razor.cs
@@ -2,11 +2,21 @@ namespace ZZZDmgCalculator.Components.Setup;
 
 using Microsoft.AspNetCore.Components;
 using Models.State;
+using Radzen;
 using Util;
 
 public partial class AgentsView {
 	[Parameter]
 	public AgentState?[] Agents { get; set; } = null!;
+
+	bool CanRemove => Agents.Count(i=>i != null) > 1;
+
+	JustifyContent Justify => State.CurrentAgentIndex switch {
+		0 => JustifyContent.Start,
+		1 => JustifyContent.Center,
+		2 => JustifyContent.End,
+		_ => JustifyContent.SpaceEvenly
+	};
 
 	async Task ChooseAgent(int li) {
 		if (await Dialogs.OpenAgentDialog() is {} a)
@@ -19,5 +29,33 @@ public partial class AgentsView {
 		if(li == State.CurrentAgentIndex) return;
 		State.CurrentAgentIndex = li;
 		Notifier.CurrentAgentChanged();
+	}
+	
+	void RemoveAgent() {
+		if(!CanRemove) return;
+		// get the index of other agent to swap to
+		var index = Agents.Select((a, i) => (a, i)).First(i => i.a != null && i.i != State.CurrentAgentIndex).i;
+		Agents[State.CurrentAgentIndex] = null;
+		State.CurrentAgentIndex = index;
+		Notifier.CurrentAgentChanged();
+	}
+	
+	void MoveRight() {
+		var newIndex = State.CurrentAgentIndex + 1;
+		if(newIndex >= Agents.Length) newIndex = 0;
+		
+		(Agents[newIndex], Agents[State.CurrentAgentIndex]) = (Agents[State.CurrentAgentIndex], Agents[newIndex]);
+		
+		State.CurrentAgentIndex = newIndex;
+	}
+	
+	void MoveLeft() {
+		var newIndex = State.CurrentAgentIndex - 1;
+		if(newIndex < 0) newIndex = Agents.Length - 1;
+		
+		(Agents[newIndex], Agents[State.CurrentAgentIndex]) = (Agents[State.CurrentAgentIndex], Agents[newIndex]);
+		
+		State.CurrentAgentIndex = newIndex;
+		
 	}
 }
